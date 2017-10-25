994b5ffecb759e889c0280ab8ae7dc3b8a0aab43
sdsdesu <sdsdesutter@live.be>
2017-10-25 13:15:52 +0200

    small Change on The Login Page

--- WishList/Login.xaml.cs
+++ WishList/Login.xaml.cs
@@ -20,9 +20,9 @@ namespace WishList
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage1 : Page
+    public sealed partial class Login : Page
     {
-        public BlankPage1()
+        public Login()
         {
             this.InitializeComponent();
         }
