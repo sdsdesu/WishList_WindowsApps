d5ee9a50cb1282cec6d3cd22dcaa2418264d0abe
sdsdesu <sdsdesutter@live.be>
2017-11-29 14:11:00 +0100

    Tried to make a Controlter

--- WislistDataCore/Models/UserModel.cs
+++ WislistDataCore/Models/UserModel.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WislistDataCore.Models
 {
-    public class UserModel
-    {
+    public class UserModel { 
+     //  [PrimariKey]
         private int UserId { get; set; }                    //id of user    //unique generated on creation
         private string Firstname { get; set; }              //name of user
         private string Lastname { get; set; }
