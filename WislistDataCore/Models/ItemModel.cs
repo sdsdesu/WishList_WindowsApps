7b81eb1388c3a5056673efb1bee296bf84639034
sdsdesu <sdsdesutter@live.be>
2017-11-29 11:02:42 +0100

    small changes in Models in datacore

--- WislistDataCore/Models/ItemModel.cs
+++ WislistDataCore/Models/ItemModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,8 @@ namespace WislistDataCore.Models
     public class ItemModel
     {
         private int ItemId { get; set; }                        //id of item
-        private string Name { get; set; }                       //name of the item
+        private string Name { get; set; }//name of the item
+        [ForeignKey("WishlistModel")]
         private string Description { get; set; }                //item description - semi optional when you give a store link
         private string WebLink { get; set; }                    //link to the item in an online store
         private string Image { get; set; }                      // hyperlink to image 
