a3bad3f4ba394fc17ebb8e16fb15ab8981a4cdde
Impling <luca.impling@gmail.com>
2017-12-14 16:30:29 +0100

    contine viewmodel

--- WishList/ViewModels/ContactViewModel.cs
+++ WishList/ViewModels/ContactViewModel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WishList.Models;
 
 namespace WishList.ViewModels
 {
     class ContactViewModel
     {
-        //contacst
+        public Wishlist selectedWishlist { get; set; }
+
+
+        public ContactViewModel(Wishlist w)
+        {
+            selectedWishlist = w;
+        }
     }
 }
