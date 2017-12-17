3013d78fb386130812ed611ca9cf00b79c617db1
sdsdesu <sdsdesutter@live.be>
2017-12-17 17:24:37 +0100

    de interne migrations

---
+++ WislistDataCore/Migrations/20171217155845_Initial.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WislistDataCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoryModel",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryModel", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserModel",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Firstname = table.Column<string>(nullable: true),
+                    Lastname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserModel", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WishListModel",
+                columns: table => new
+                {
+                    ListId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Deadline = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WishListModel", x => x.ListId);
+                    table.ForeignKey(
+                        name: "FK_WishListModel_UserModel_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserModel",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemModel",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Categoryid = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Image = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    WebLink = table.Column<string>(nullable: true),
+                    WishListId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemModel", x => x.ItemId);
+                    table.ForeignKey(
+                        name: "FK_ItemModel_CategoryModel_Categoryid",
+                        column: x => x.Categoryid,
+                        principalTable: "CategoryModel",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemModel_WishListModel_WishListId",
+                        column: x => x.WishListId,
+                        principalTable: "WishListModel",
+                        principalColumn: "ListId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemModel_Categoryid",
+                table: "ItemModel",
+                column: "Categoryid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemModel_WishListId",
+                table: "ItemModel",
+                column: "WishListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishListModel_UserId",
+                table: "WishListModel",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemModel");
+
+            migrationBuilder.DropTable(
+                name: "CategoryModel");
+
+            migrationBuilder.DropTable(
+                name: "WishListModel");
+
+            migrationBuilder.DropTable(
+                name: "UserModel");
+        }
+    }
+}
---
+++ WislistDataCore/Migrations/WislistDataCoreContextModelSnapshot.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using WislistDataCore.Models;
+
+namespace WislistDataCore.Migrations
+{
+    [DbContext(typeof(WislistDataCoreContext))]
+    partial class WislistDataCoreContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("WislistDataCore.Models.CategoryModel", b =>
+                {
+                    b.Property<int>("CategoryID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("CategoryName");
+
+                    b.HasKey("CategoryID");
+
+                    b.ToTable("CategoryModel");
+                });
+
+            modelBuilder.Entity("WislistDataCore.Models.ItemModel", b =>
+                {
+                    b.Property<int>("ItemId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Categoryid");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Image");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("WebLink");
+
+                    b.Property<int>("WishListId");
+
+                    b.HasKey("ItemId");
+
+                    b.HasIndex("Categoryid");
+
+                    b.HasIndex("WishListId");
+
+                    b.ToTable("ItemModel");
+                });
+
+            modelBuilder.Entity("WislistDataCore.Models.UserModel", b =>
+                {
+                    b.Property<int>("UserId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Firstname");
+
+                    b.Property<string>("Lastname");
+
+                    b.HasKey("UserId");
+
+                    b.ToTable("UserModel");
+                });
+
+            modelBuilder.Entity("WislistDataCore.Models.WishListModel", b =>
+                {
+                    b.Property<int>("ListId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Deadline");
+
+                    b.Property<string>("Title");
+
+                    b.Property<int>("UserId");
+
+                    b.HasKey("ListId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("WishListModel");
+                });
+
+            modelBuilder.Entity("WislistDataCore.Models.ItemModel", b =>
+                {
+                    b.HasOne("WislistDataCore.Models.CategoryModel", "Category")
+                        .WithMany()
+                        .HasForeignKey("Categoryid")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("WislistDataCore.Models.WishListModel", "WishList")
+                        .WithMany("Items")
+                        .HasForeignKey("WishListId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("WislistDataCore.Models.WishListModel", b =>
+                {
+                    b.HasOne("WislistDataCore.Models.UserModel", "User")
+                        .WithMany()
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
